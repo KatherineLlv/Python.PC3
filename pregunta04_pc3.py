8eaf18b9d51975c1d4dd15b58a2d330b18299d77
KatherineLlv <168352561+KatherineLlv@users.noreply.github.com>
2024-05-25 16:08:36 -0500

    Add files via upload

---
+++ pregunta04_pc3.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 25 14:49:07 2024
+
+@author: Katherine
+"""
+# Pregunta 03:
+
+def RECTANGULO(largo, ancho):
+    return largo*ancho
+
+
+area = RECTANGULO(3, 10)
+print("El área del rectángulo es "+str(area))
+
