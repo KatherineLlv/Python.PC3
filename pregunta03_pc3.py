43e45a3bf995f1029615a071bf291e6613092c27
KatherineLlv <168352561+KatherineLlv@users.noreply.github.com>
2024-05-25 16:06:04 -0500

    Add files via upload

---
+++ pregunta03_pc3.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 25 14:49:07 2024
+
+@author: Katherine
+"""
+# Pregunta 03:
+
+def circulo(radio):
+    return 3.14*(radio**2)
+
+
+area = circulo(2)
+print("El área del circulo es "+str(area))
+
