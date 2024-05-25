91fc5244f3e32d540fd98416d4169abf1e9c0579
KatherineLlv <168352561+KatherineLlv@users.noreply.github.com>
2024-05-25 16:20:43 -0500

    Add files via upload

---
+++ pregunta05_pc3.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 25 14:49:07 2024
+
+@author: Katherine
+"""
+# Pregunta 05:
+
+
+class Alumno:
+    pass
+
+
+display = Alumno()
+
+display.nombre = "Katherine"
+display.numero.registro = "1"
+
+
+setAge = Alumno()
+
+setAge.edad = "21"
+
+setNota = Alumno()
+
+setNota.nota01 = 16
+setNota.nota02 = 10
+setNota.nota03 = 15
+setNota.nota04 = 20
+
